5a0f2dda8e59097f9cb4b7b48f24c7d8871ad982
Dopamine-TX <1650332175@qq.com>
2021-10-07 19:07:51 +0800

    Update endnote_plus.py

--- 项目/endnote_plus.py
+++ 项目/endnote_plus.py
@@ -18,11 +18,6 @@ import pickle
 
 
 Item = collections.namedtuple('Item', 'abb all_w')
-# tree=[]
-# for i in range(0,26):
-#     alphabet=chr(ord('a')+i)
-#     tree.append(pickle.load(open('terms/tree.'+alphabet,'rb')))
-# tree.append(pickle.load(open('terms/tree._','rb')))
 tree=pickle.load(open('terms/terms.tree','rb'))
 
 
@@ -54,11 +49,6 @@ def normalize(s):
 
 '''获取期刊全名'''
 def get_journal(term):
-    i=ord(term[0].lower())-ord('a')
-#     if i in range(0,26):
-#         t=sorted(tree[i].find(Item(term, term), 100))
-#     else:
-#         t=sorted(tree[26].find(Item(term, term), 100))
     t=sorted(tree.find(Item(term, term), 0))#！！！这里调节。！！！
     try:#不一定有解
         print(t[0])
@@ -75,20 +65,15 @@ def get_journal(term):
 def title_handle(c):
     c.execute('SELECT title FROM refs')
     titles=c.fetchall()
-
     i=1
     for title in titles:
         title=normalize(str(title[0]))
-        command='UPDATE refs SET title = "'+title+ '" where ID='+str(i)            
-#         print(command)
-#         print(chardet.detect(str.encode(command)))
-        c.execute(command)
-#         try:
-#             c.execute(command)
-#             print(command)
-#         except:
-#             print('错误！')
-#这里全报错，所以就注释掉了
+        command='UPDATE refs SET title = "'+title+ '" where ID='+str(i)
+        try:
+            c.execute(command)
+            print(command)
+        except:
+            print('错误！')
         i+=1    
 
 
@@ -103,26 +88,10 @@ def journals_handle(c):
         if journal[0]:
             print(journal[0])
             journal_=get_journal(journal[0])
-            command='UPDATE refs SET secondary_title = "'+journal_+ '" where ID='+str(i)            
-#       title=normalize(str(title[0]))
-#         print(chardet.detect(str.encode(command)))
+            command='UPDATE refs SET secondary_title = "'+journal_+ '" where ID='+str(i)
             c.execute(command)
 
 
-# In[48]:
-
-
-# Create table c.execute('''CREATE TABLE stocks(date text, trans text, symbol text, qty real, price real)''')
-
-# Insert a row of data c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
-# Save (commit) the changes conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost. conn.close()
-
-
-# In[49]:
 
 
 if __name__ == '__main__':
@@ -147,10 +116,6 @@ if __name__ == '__main__':
     conn = sqlite3.connect('sdb2.db')
     c = conn.cursor()
     
-    #sqlite3.dump()没有这样的接口
-    #conn.create_collation('ENCIN_zh_CN', my_collate)
-    #endnote可能自定义了这个排序规则，所以我补了一下，没想到它还有更多自定义的东西（
-    
 ##获取目标表名
 #     c.execute('SELECT name FROM sqlite_master WHERE type="table" order by name')
 #     table_names=c.fetchall()
@@ -163,27 +128,9 @@ if __name__ == '__main__':
 #     print(c.fetchall())
     
     #论文名格式处理
-    journals_handle(c)#title_handle(c)
-    
+    title_handle(c)
+    journals_handle(c)
+
     conn.commit()
-    
-
-
-# In[25]:
-
-
-c.execute('PRAGMA table_info(refs)')
-print(c.fetchall())
-
-
-# In[52]:
-
-
-print(sorted(tree.find(Item("International Conference on Mathematical Modeling in Physical Science"," term"), 50))[0])
-
-
-# In[ ]:
-
-
-conn.close()
+    conn.close()
 
